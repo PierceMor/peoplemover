eedc521e842de1c29fa3e944c14de64ee6151b12
Pierce Morrill <piercemorrill91@gmail.com>
2018-08-25 09:50:39 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = proccess.env.PORT || 3001;
+
+//define middleware
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+// server up static assets 
+if (process.env.NODE_ENV === 'production' ){
+    app.use(express.static('client/build'));
+}
+
+app.use(routes);
+
+mongoose.connext(process.env.MONGODB_URL || "mongodb://localhost" );
+
+app.listen(PORT, function(){
+    console.log( `API Server now listening on PORT ${PORT}!`);
+});
